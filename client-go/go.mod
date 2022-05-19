63dc980ed24fefc1a9eac4dbc4e276af6a71b516
mephostopilis <mephostopilis@gmail.com>
2022-05-19 14:28:45 +0800

    init :+1:

---
+++ client-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/music-solana/client-go
+
+go 1.18
---
+++ program/src/entrypoint.rs
@@ -0,0 +1,21 @@
+//! Program entrypoint
+
+#![cfg(not(feature = "no-entrypoint"))]
+
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::{ProgramResult, HEAP_LENGTH, HEAP_START_ADDRESS},
+    pubkey::Pubkey,
+};
+
+use std::{alloc::Layout, mem::size_of, ptr::null_mut};
+
+entrypoint!(process_instruction);
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    crate::processor::process_instruction(program_id, accounts, instruction_data)
+}
