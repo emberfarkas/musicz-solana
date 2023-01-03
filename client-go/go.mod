d2ebc1739054988474e2966f2379b61bf1b6a41f
emberfarkas <emberfarkas@hotmail.com>
2023-01-03 08:55:25 +0800

    evm

--- client-go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/music-solana/client-go
-
-go 1.18
---
+++ evm/src/main.rs
@@ -0,0 +1,22 @@
+use clap::Parser;
+use log::{error, info};
+use tokio::sync::mpsc;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+#[tokio::main]
+async fn main() {
+    simplelog::SimpleLogger::init(log::LevelFilter::Debug, simplelog::Config::default()).unwrap();
+    println!("Hello, world!");
+}
